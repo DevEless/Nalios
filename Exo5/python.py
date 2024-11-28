5e037d4ea4f6727ffa437e42878e72b302db7955
Develess <qdelameilleure@gmail.com>
2024-11-28 19:29:56 +0100

    exo 5 done

--- Exo5/python.py
+++ Exo5/python.py
@@ -1,4 +1,14 @@
 def sum(nombres):
     if not nombres:
+        #si la liste est vide, return 0
         return 0
     return nombres[0] + sum(nombres[1:])
+#nombres[0] indique l'index du premier element , nombres[1:] prends en compte tous les elements sauf le premier
+#la fonction ajoute le premier element a la sommes des autres
+
+
+#test
+
+nombres = [2.5, 4.0, -2.0, 11.0]
+result = sum(nombres)
+print({result})
