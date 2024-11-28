c4784677fb2ea20b3dad73e763a44928b5fab440
Develess <qdelameilleure@gmail.com>
2024-11-28 19:05:57 +0100

    exo3 done

--- Exo3/main.js
+++ Exo3/main.js
@@ -1,12 +1,15 @@
 function HelloNalios(){
-    // j'ai cherché et franchement c'est sympa, merci
+
 
     //stock dans une variable la methode qui va transformer le code ASCII en lettre et caractere
     let message = String.fromCharCode(
-        //code ASCII qui va définir chaque lettre et caractere
-    72
+        72, 101, 108, 108, 111, 44, // "Hello,"
+        32, 78, 97, 108, 105, 111, 115, // " Nalios"
+        32, 33 // "!"
     );
+// affiche le message dans la console
     console.log(message)
 }
 
 HelloNalios()
+//lance la fonction
