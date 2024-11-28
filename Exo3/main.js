d9b747455cad34ecd860c5ef576263ee2e70caad
Develess <qdelameilleure@gmail.com>
2024-11-28 19:04:24 +0100

    js first step

---
+++ Exo3/main.js
@@ -0,0 +1,12 @@
+function HelloNalios(){
+    // j'ai cherché et franchement c'est sympa, merci
+
+    //stock dans une variable la methode qui va transformer le code ASCII en lettre et caractere
+    let message = String.fromCharCode(
+        //code ASCII qui va définir chaque lettre et caractere
+    72
+    );
+    console.log(message)
+}
+
+HelloNalios()
