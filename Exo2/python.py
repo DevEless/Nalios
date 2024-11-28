306acc9c8e98f8848612c59b909c0bb003ba8f1c
Develess <qdelameilleure@gmail.com>
2024-11-28 18:43:09 +0100

    condition born, die , reproduct of cells

--- Exo2/python.py
+++ Exo2/python.py
@@ -19,4 +19,23 @@ def gameoflife(matrix, iteration=5):
         return voisin
     
 
-    
+    def nextstep(matrix):
+        rows,cols = len(matrix), len(matrix[0])
+        new_matrix = [[0] * cols for _ in range(rows)]
+
+        for x in range(rows):
+            for y in range(cols):
+                voisin_vivants = voisin(x,y, rows, cols)
+# en prenant en compte les regles , une cellule vivante meurt si elle a moins de deux voisins en vie , ou plus de 3
+                if matrix[x][y] == 1:
+                    if voisin_vivants < 2 or voisin_vivants > 3:
+                        #verifie si il y a une sous ou une sur population
+                        new_matrix[x][y] = 0 #rénitialise a 0 donc il meurt
+                    else:
+                        new_matrix[x][y] = 0 #reste en vie
+                else:
+                    #si la cellule a 3 voisins en vie elle en crée une nouvelle par reproduction
+                    if voisin_vivants == 3:
+                        new_matrix[x][y] = 1 #nait
+            return new_matrix
+        
